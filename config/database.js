7d438d5115d474af12002a73d52f04079cc3c244
Saksham Walia <145364995+SakshamWalia03@users.noreply.github.com>
2025-07-28 22:08:39 +0530

    Updated database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,28 @@
-// config/database.js
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
-import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  logging: false,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: process.env.DB_DIALECT || 'mysql',
+        logging: false,
+      }
+    );
 
 export default sequelize;
