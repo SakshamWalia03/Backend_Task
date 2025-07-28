0cdb20e24a6cccc47484dd36c6c26f994bd42276
Saksham Walia <145364995+SakshamWalia03@users.noreply.github.com>
2025-07-28 22:18:01 +0530

    Added get request for the base url

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Form Application API');
+});
+
 app.use('/api', formRoutes);
 
 app.use(errorHandler);
